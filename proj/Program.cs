b834c542f9146337e5ae2fd1aa9f0b5db78e3405
Falah Sulaiman Ranardy <falahsulaimanranardy@gmail.com>
2025-11-27 07:42:44 +0700

    Inisialisasi awal project

---
+++ proj/Program.cs
@@ -0,0 +1,21 @@
+﻿// Kelas            : S1SI-KJ-25-06
+// Kelompok         : 01
+// Anggota Kelompok :
+// 1. Falah Sulaiman Ranardy (102042500080)
+// 2. Masayu Kayla Thalita (102042500066)
+// 3. Meisya Dwi Alika (102042500166)
+// 4. Fathuriza Rahman (102042530031)
+// 5. Deden Saputra (102042500119)
+// 6. Kiandra Narashreya Adji Prasdhian (102042500107)
+// 7. Rafi Maulana Elfajar (102042500211)
+
+namespace TubesAlpro_Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
