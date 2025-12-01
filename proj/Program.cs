c462917337d28054d934a666cc09b9dab95bbfd9
Falah Sulaiman Ranardy <falahsulaimanranardy@gmail.com>
2025-12-01 22:15:20 +0700

    Update Program.cs

--- proj/Program.cs
+++ proj/Program.cs
@@ -1,13 +1,13 @@
-﻿﻿// Kelas            : S1SI-KJ-25-06
+﻿// Kelas            : S1SI-KJ-25-06
 // Kelompok         : 01
 // Anggota Kelompok :
-// 1. Masayu Kayla Thalita (102042500066)
-// 2. Falah Sulaiman Ranardy (102042500080)
+// 1. Masayu Kayla Thalita              (102042500066)
+// 2. Falah Sulaiman Ranardy            (102042500080)
 // 3. Kiandra Narashreya Adji Prasdhian (102042500107)
-// 4. Deden Shaputra (102042500119)
-// 5. Meisya Dwi Alika (102042500166)
-// 6. Rafi Maulana Elfajar (102042500211)
-// 7. Fathuriza Rahman (102042530031)
+// 4. Deden Shaputra                    (102042500119)
+// 5. Meisya Dwi Alika                  (102042500166)
+// 6. Rafi Maulana Elfajar              (102042500211)
+// 7. Fathuriza Rahman                  (102042530031)
 using System;
 
 namespace TubesAlpro_DataMahasiswa
